a6ccb2b7aa51241da306d4ee1a81935b953f83f2
Tho Nguyen <thogotmail@gmail.com>
2015-07-08 18:27:15 -0500

    add test for create action

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -7,4 +7,38 @@ describe MoviesController do
       expect(assigns(:movie)).to be_a_new Movie
     end
   end
+
+  describe 'POST create' do
+    context 'with valid parameters' do
+      before { post :create, movie: { title: 'Jurassic World', format: 'DVD', length: 300, release_year: 2015 } }
+      
+      it 'creates a movie' do
+        expect(Movie.count).to eq 1
+      end
+
+      it 'redirects to the new template' do
+        expect(response).to redirect_to new_movie_path
+      end
+
+      it 'sets the flash success message' do
+        expect(flash[:success]).to be_present
+      end
+    end
+
+    context 'with invalid parameters' do
+      before { post :create, movie: { title: 'Jurassic World' } }
+
+      it 'does not create a movie' do
+        expect(Movie.count).to eq 0
+      end
+
+      it 'renders the new template' do
+        expect(response).to render_template :new
+      end
+
+      it 'sets the flash danger message' do
+        expect(flash[:danger]).to be_present
+      end
+    end
+  end
 end

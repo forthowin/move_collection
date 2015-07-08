e454dfa15bbfaafa59e005dae64fbc8b7edd3f51
Tho Nguyen <thogotmail@gmail.com>
2015-07-08 18:26:57 -0500

    implement create action

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,4 +2,21 @@ class MoviesController < ApplicationController
   def new
     @movie = Movie.new
   end
+
+  def create
+    movie = Movie.new(movie_param)
+    if movie.save
+      flash[:success] = 'The movie was saved successfully.'
+      redirect_to new_movie_path
+    else
+      flash.now[:danger] = 'Movie was not saved due to some errors.'
+      render :new
+    end
+  end
+
+  private
+
+  def movie_param
+    params.require(:movie).permit(:title, :format, :length, :release_year, :rating)
+  end
 end

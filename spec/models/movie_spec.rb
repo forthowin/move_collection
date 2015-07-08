d0d0fdfd03ea4814da614575d489d07aeee2f6f1
Tho Nguyen <thogotmail@gmail.com>
2015-07-08 17:05:22 -0500

    fix typo and remove presence validation for rating

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -2,10 +2,9 @@ require 'rails_helper'
 
 describe Movie do
   it { should validate_presence_of :title }
-  it { should validate_prsence_of :format }
-  it { should validate_prsence_of :length }
-  it { should validate_prsence_of :release_year }
-  it { should validate_prsence_of :rating }
+  it { should validate_presence_of :format }
+  it { should validate_presence_of :length }
+  it { should validate_presence_of :release_year }
 
   it { should validate_length_of(:title).is_at_least(1).is_at_most(50) }
   it { should validate_inclusion_of(:format).in_array(%w(VHS DVD Streaming)) }

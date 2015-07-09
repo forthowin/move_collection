ba86586bf931bdaca011c5c925b910999f8e0f83
Tho Nguyen <thogotmail@gmail.com>
2015-07-09 13:08:51 -0500

    create helper method for formating movie length

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def format_rating(movie)
     movie.rating.nil? ? "-" : "#{movie.rating}/5"
   end
+
+  def format_length(length)
+    "#{length / 60} hr #{length % 60} m"
+  end
 end

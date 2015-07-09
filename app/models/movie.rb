eedc1a5286ec785e782049267f565aa37bcc936d
Tho Nguyen <thogotmail@gmail.com>
2015-07-09 14:22:56 -0500

    make error messages be more accurate

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,6 @@
 class Movie < ActiveRecord::Base
   validates :title, presence: true, length: { in: 1..50 }
   validates :format, presence: true, inclusion: { in: %w(VHS DVD Streaming) }
-  validates :length, presence: true, inclusion: { in: 1..500 }
-  validates :release_year, presence: true, inclusion: { in: 1800..2100 }
+  validates :length, presence: true, inclusion: { in: 1..500, message: 'must be between 1-500' }
+  validates :release_year, presence: true, inclusion: { in: 1800..2100, message: 'must be between 1800-2100' }
 end

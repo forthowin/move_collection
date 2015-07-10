2ee3f329d0000077d1f5b0fd278ae4a0296f6360
Tho Nguyen <thogotmail@gmail.com>
2015-07-10 10:35:18 -0500

    change route to accomidate twitter login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
   
-  get '/auth/facebook/callback', to: 'sessions#create'
+  get '/auth/:provider/callback', to: 'sessions#create'
   get 'log-out', to: 'sessions#destroy'
 
   resources :movies

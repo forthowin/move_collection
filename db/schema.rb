bffd1ece85749ec7a822189e8f1dacf3360f742e
Tho Nguyen <thogotmail@gmail.com>
2015-07-10 11:27:42 -0500

    add user_id column to collections table

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150710142410) do
+ActiveRecord::Schema.define(version: 20150710154606) do
 
   create_table "collections", force: :cascade do |t|
     t.integer  "movie_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "movies", force: :cascade do |t|

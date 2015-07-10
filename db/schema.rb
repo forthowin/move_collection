85293f3833f57857b9b3fb570cd2758edefaedd6
Tho Nguyen <thogotmail@gmail.com>
2015-07-10 10:10:30 -0500

    create users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150710031001) do
+ActiveRecord::Schema.define(version: 20150710142410) do
 
   create_table "collections", force: :cascade do |t|
     t.integer  "movie_id"
@@ -28,6 +28,16 @@ ActiveRecord::Schema.define(version: 20150710031001) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "cover"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
+    t.string   "oauth_token"
+    t.datetime "oauth_expires_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end

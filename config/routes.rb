47bf28983ad1bdc4553d157a24a08bd40844f5e2
Tho Nguyen <thogotmail@gmail.com>
2015-07-10 10:12:30 -0500

    create routes for facebook login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'pages#home'
   
+  get '/auth/facebook/callback', to: 'sessions#create'
+  get 'log-out', to: 'sessions#destroy'
+
   resources :movies
   resources :collections, only: [:create, :index, :destroy]
 end
